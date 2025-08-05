88e5ab2bbc6f03436bd016c1c732642819b8b378
sanathsadiga <sanathadigas@gmail.com>
2025-08-05 14:53:32 +0530

    Initial commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://voyage-forge.com/', // Replace with your actual domain
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
